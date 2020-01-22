dffb3eaf796478acd93000abb90494c61dec9e04
WordsPerMinute <contact@coreyhodge.net>
2020-01-22 13:10:50 -0700

    migrated the databases

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_22_012054) do
 
+  create_table "fighter_moves", force: :cascade do |t|
+    t.integer "fighter_id"
+    t.integer "move_id"
+    t.index ["fighter_id"], name: "index_fighter_moves_on_fighter_id"
+    t.index ["move_id"], name: "index_fighter_moves_on_move_id"
+  end
+
+  create_table "fighters", force: :cascade do |t|
+    t.string "name"
+    t.integer "hp"
+    t.integer "attack"
+    t.integer "defense"
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.string "name"
+    t.integer "base_damage"
+    t.integer "minimum_to_hit"
+    t.integer "when_to_critical"
+    t.integer "when_to_damage_self"
+  end
+
+  add_foreign_key "fighter_moves", "fighters"
+  add_foreign_key "fighter_moves", "moves"
 end
